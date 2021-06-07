c7c73045eeaef4d9e9b57320897cdc0fa6f8a153
Piyopanman <g19911kh@gm.tsuda.ac.jp>
2021-06-08 00:28:16 +0900

    next.config.jsの修正

--- next.config.js
+++ next.config.js
@@ -2,10 +2,11 @@ const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
 });
 const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
 
 module.exports = withPWA({
   pwa: {
     dest: "public",
-    runtimeCashing,
+    runtimeCaching,
   },
 });
